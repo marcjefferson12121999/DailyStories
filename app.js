d77f0d6f721ff12f2d2e9f0817200533a2fcdff7
Marc Jefferson <sumilangmj@gmail.com>
2020-11-17 21:41:06 +0800

    six attempt

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require("express")
 const dotenv = require ('dotenv')
 const morgan = require ('morgan')
@@ -19,6 +20,10 @@ if (process.env.NODE_ENV === 'development'){
 //Handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+//static folder
+
+app.use(express.static(path.join(__dirname,'public')))
 
 //Routes
 app.use('/', require ('./routes/index'))
